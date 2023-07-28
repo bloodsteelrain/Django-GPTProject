9921a98d88b387cb5dbd1ffcb4e56945e2fc4317
bloodsteelrain <chwoo9528@gmail.com>
2023-07-28 18:17:01 +0900

    챗봇 API 구현 및 대화내역 데이터베이스 연결

--- chat_project/urls.py
+++ chat_project/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
+    path('chatbot/', include('chatbot.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from django.contrib.auth import get_user_model
+from .models import Conversation
+from dotenv import load_dotenv
+import openai
+import os
 
-# Create your views here.
+User = get_user_model()
+
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+class ChatbotView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        conversations = Conversation.objects.filter(user=user)
+        serialized_conversations = [{'prompt': c.prompt, 'response': c.response} for c in conversations]
+        return Response({'conversations': serialized_conversations}, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        prompt = request.data.get('prompt')
+        if prompt:
+            user = request.user
+
+            # 데이터베이스에서 과거 대화들 가져오기
+            previous_conversations = Conversation.objects.filter(user=user)
+            previous_conversations_text = "\n".join([f"User: {c['prompt']}\nAI: {c['response']}" for c in previous_conversations])
+            
+            # 과거 대화들을 새 질문과 합치기
+            prompt_with_previous = f"{previous_conversations_text}\nUser: {prompt}\nAI:"
+
+            # OpenAI를 사용해 AI의 답변을 생성하기
+            model_engine = "text-davinci-003"
+            completions = openai.Completion.create(
+                engine=model_engine,
+                prompt=prompt_with_previous,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                temperature=0.5,
+            )
+            response = completions.choices[0].text.strip()
+
+            # 새 질문(prompt)과 답변(response)을 데이터베이스에 저장하기
+            conversation = Conversation.objects.create(prompt=prompt, response=response, user=user)
+
+            return Response({'prompt': prompt, 'response': response}, status=status.HTTP_200_OK)
+        return Response({'error': 'Prompt field is required.'}, status=status.HTTP_400_BAD_REQUEST)
