47201efaa01474395990f253f11f1a2ea00a921f
bloodsteelrain <chwoo9528@gmail.com>
2023-09-23 21:29:30 +0900

    drf에 필요없는 template 관련 세팅 삭제

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'chat_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
